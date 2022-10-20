ce9c6c350a1f23fb4ff794ac18c55ff2f1413250
Ivan <103282550+sizze1veG@users.noreply.github.com>
2022-10-20 15:01:38 +0500

    Add files via upload

---
+++ ApartmentExchangeBureau/ApartmentExchangeBureau/MainForm.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ApartmentExchangeBureau
+{
+    public partial class MainForm : Form
+    {
+        public List<Flat> flats = new List<Flat>();
+        public int index = 0;
+        public bool flag = false;
+        public string flat2Address;
+        public string flat2Room;
+        public string flat2Floor;
+        public string flat2Area;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string file = @"DataBaseFlats.txt";
+            if (File.Exists(file))
+            {
+                StreamReader f = new StreamReader("DataBaseFlats.txt", Encoding.GetEncoding(1251));
+                while (!f.EndOfStream)
+                {
+                    string s = f.ReadLine();
+                    string[] data = s.Split('\t');
+                    Flat flat = new Flat();
+                    flat.Rooms = int.Parse(data[0]);
+                    flat.Floor = int.Parse(data[1]);
+                    flat.Area = Convert.ToDouble(data[2]);
+                    flat.Address = data[3];
+                    flats.Add(flat);
+                    dataGridView1.Rows.Add(data);
+                }
+                f.Close();
+            }  
+        }
+
+        private void buttonGetapplication_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            string countRoom = textBoxRooms.Text;
+            string numberFloor = textBoxFloor.Text;
+            string areaFlat = textBoxArea.Text;
+            string addressFlat = textBoxAddress.Text;
+            flat2Room = countRoom;
+            flat2Floor = numberFloor;
+            flat2Area = areaFlat;
+            flat2Address = addressFlat;
+            if (CheckCountRooms(countRoom) && CheckNumberFloor(numberFloor) && CheckArea(areaFlat))
+            {
+                List<Flat> flat = new List<Flat>();
+                foreach (var item in flats)
+                {
+                    if (int.Parse(countRoom) == item.Rooms && int.Parse(numberFloor) == item.Floor && GetProcent(Convert.ToDouble(areaFlat), item.Area))
+                    {
+                        flat.Add(item);
+                    }
+                }
+                if (flat.Count > 0)
+                {
+                    SituableApartments form = new SituableApartments(flat);
+                    form.Owner = this;
+                    form.ShowDialog();
+                    if (!flag)
+                    {
+                        dataGridView1.Rows.RemoveAt(index);
+                        RewriteTxt();
+                    }
+                    else
+                    {
+                        AddFlat(countRoom, numberFloor, areaFlat, addressFlat);
+                        MessageBox.Show("Квартира не выбрана! Заявка добавлена в список.");
+                    }
+                }
+                else
+                {
+                    AddFlat(countRoom, numberFloor, areaFlat, addressFlat);
+                    MessageBox.Show("Подходящих квартир нет! Заявка добавлена в список.");
+                }
+                textBoxRooms.Clear();
+                textBoxFloor.Clear();
+                textBoxArea.Clear();
+                textBoxAddress.Clear();
+            }
+            else
+            {
+                if (!CheckCountRooms(countRoom))
+                {
+                    MessageBox.Show("Количество комнат введено неверно!");
+                }
+                else if (!CheckNumberFloor(numberFloor))
+                {
+                    MessageBox.Show("Этаж введён неверно!");
+                }
+                else if (!CheckArea(areaFlat))
+                {
+                    MessageBox.Show("Площадь введена неверно");
+                }
+            }
+        }
+
+        private bool CheckCountRooms(string countRoom)
+        {
+            if (countRoom.Length < 1 || countRoom.Length > 2)
+            {
+                return false;
+            }
+            foreach (var item in countRoom)
+            {
+                if (!char.IsNumber(item))
+                {
+                    return false;
+                }
+            }
+            if (int.Parse(countRoom) > 10)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool CheckNumberFloor(string numberFloor)
+        {
+            if (numberFloor.Length < 1 || numberFloor.Length > 2)
+            {
+                return false;
+            }
+            foreach (var item in numberFloor)
+            {
+                if (!char.IsNumber(item))
+                {
+                    return false;
+                }
+            }
+            if (int.Parse(numberFloor) > 40)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool CheckArea(string areaFlat)
+        {
+            if (areaFlat.Length < 2 || areaFlat.Length > 6)
+            {
+                return false;
+            }
+            foreach (var item in areaFlat)
+            {
+                if (!char.IsNumber(item) && item != '.')
+                {
+                    return false;
+                }
+            }
+            if (Convert.ToDouble(areaFlat) > 300)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool GetProcent(double flat1, double flat2)
+        {
+            if (flat1 == flat2)
+            {
+                return true;
+            }
+            else if (flat1 > flat2)
+            {
+                if (Convert.ToInt32((flat1 - flat2) / flat1 * 100) <= 10)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (Convert.ToInt32((flat2 - flat1) / flat2 * 100) <= 10)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private void RewriteTxt()
+        {
+            StreamWriter sw = new StreamWriter("DataBaseFlats.txt", false, Encoding.GetEncoding(1251));
+            foreach (var item in flats)
+            {
+                sw.WriteLine(item.Rooms + "\t" + item.Floor + "\t" + item.Area + "\t" + item.Address);
+            }
+            sw.Close();
+        }
+
+        private void AddFlat(string countRoom, string numberFloor, string areaFlat, string addressFlat)
+        {
+            dataGridView1.Rows.Add(countRoom, numberFloor, areaFlat, addressFlat);
+            Flat flat1 = new Flat();
+            flat1.Rooms = int.Parse(countRoom);
+            flat1.Floor = int.Parse(numberFloor);
+            flat1.Area = Convert.ToDouble(areaFlat);
+            flat1.Address = addressFlat;
+            flats.Add(flat1);
+            RewriteTxt();
+        }
+    }
+}
---
+++ ApartmentExchangeBureau/ApartmentExchangeBureau/SituableApartments.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ApartmentExchangeBureau
+{
+    public partial class SituableApartments : Form
+    {
+        List<Flat> flat = new List<Flat>();// список подходящих квартир
+        Flat flat1;
+        Flat flat2;
+
+        public SituableApartments(List<Flat> flat)
+        {
+            InitializeComponent();
+            this.flat = flat;
+            MainForm main = Owner as MainForm;
+        }
+
+        private void SituableApartments_Load(object sender, EventArgs e)
+        {
+            MainForm main = Owner as MainForm;
+            foreach (var item in flat)
+            {
+                dataGridView1.Rows.Add(item.Rooms, item.Floor, item.Area, item.Address);
+            }       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm main = Owner as MainForm;
+            Close();
+            main.flag = true;
+        }
+
+        private void buttonApplication_Click(object sender, EventArgs e)
+        {
+            MainForm main = Owner as MainForm;
+            int index = dataGridView1.CurrentRow.Index;
+            int index1 = 0;
+            Flat apart = new Flat();
+            for (int i = 0; i < flat.Count; i++)
+            {
+                if (i == index)
+                {
+                    apart = flat.ElementAt(i);
+                    flat1 = apart;
+                }
+            }
+            dataGridView1.Rows.RemoveAt(index);
+            foreach (var item in main.flats)
+            {
+                if (item.Rooms == apart.Rooms && item.Floor == apart.Floor && item.Area == apart.Area && item.Address == apart.Address)
+                {
+                    main.flats.Remove(item);
+                    break;
+                }
+                index1++;
+            }
+            flat2.Address = main.flat2Address;
+            flat2.Rooms = int.Parse(main.flat2Room);
+            flat2.Floor = int.Parse(main.flat2Floor);
+            flat2.Area = Convert.ToDouble(main.flat2Area);
+            main.index = index1;
+            MessageBox.Show("Заявка принята и сформирована в PDF-файл!");
+            PDF();
+            Close();
+        }
+
+        private void PDF()
+        {
+            var document = new Document(PageSize.A4, 20, 20, 30, 20);
+
+            string ttf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "ARIALNBI.TTF");
+            var baseFont = BaseFont.CreateFont(ttf, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            var font = new iTextSharp.text.Font(baseFont, iTextSharp.text.Font.DEFAULTSIZE, iTextSharp.text.Font.NORMAL);
+
+
+            using (var writer = PdfWriter.GetInstance(document, new FileStream("Заявка.pdf", FileMode.Create)))
+            {
+                document.Open();
+                document.NewPage();
+
+                document.Add(new Paragraph("\t\t\tБюро обмена квартир \"Винзили\"\n", font));
+                document.Add(new Paragraph("\t\t\t\t\t\t \nРиелтор: Топычканов Иван\n", font));
+                document.Add(new Paragraph("\n\nЗаявка на обмен квартир", font));
+                document.Add(new Paragraph($"1.\nАдрес:{flat1.Address}\nКоличество комнат:{flat1.Rooms}\nЭтаж:{flat1.Floor}\nПлощадь:{flat1.Area}", font));
+                document.Add(new Paragraph($"\n2.\nАдрес:{flat2.Address}\nКоличество комнат:{flat2.Rooms}\nЭтаж:{flat2.Floor}\nПлощадь:{flat2.Area}", font));
+
+                document.Close();
+                writer.Close();
+            }
+        }
+    }
+}

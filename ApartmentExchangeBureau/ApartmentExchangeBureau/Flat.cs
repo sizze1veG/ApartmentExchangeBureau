d6a0c2d2810b476afdb217fbd9732b069e9c386d
Ivan <103282550+sizze1veG@users.noreply.github.com>
2023-02-14 16:51:39 +0500

    Add files via upload

---
+++ ApartmentExchangeBureau/ApartmentExchangeBureau/Flat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ApartmentExchangeBureau
+{
+    public struct Flat
+    {
+        private int rooms;
+
+        public int Rooms
+        {
+            get { return rooms; }
+            set { rooms = value; }
+        }
+
+        private int floor;
+
+        public int Floor
+        {
+            get { return floor; }
+            set { floor = value; }
+        }
+
+        private double area;
+
+        public double Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3}", rooms, floor, area, address);
+        }
+    }
+}
---
+++ ApartmentExchangeBureau/ApartmentExchangeBureau/MainForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace ApartmentExchangeBureau
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxRooms = new System.Windows.Forms.TextBox();
+            this.textBoxFloor = new System.Windows.Forms.TextBox();
+            this.textBoxArea = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.buttonGetapplication = new System.Windows.Forms.Button();
+            this.labelRooms = new System.Windows.Forms.Label();
+            this.labelFloor = new System.Windows.Forms.Label();
+            this.labelArea = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(84, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(523, 339);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBoxRooms
+            // 
+            this.textBoxRooms.Location = new System.Drawing.Point(12, 392);
+            this.textBoxRooms.Name = "textBoxRooms";
+            this.textBoxRooms.Size = new System.Drawing.Size(118, 20);
+            this.textBoxRooms.TabIndex = 1;
+            // 
+            // textBoxFloor
+            // 
+            this.textBoxFloor.Location = new System.Drawing.Point(172, 392);
+            this.textBoxFloor.Name = "textBoxFloor";
+            this.textBoxFloor.Size = new System.Drawing.Size(118, 20);
+            this.textBoxFloor.TabIndex = 2;
+            // 
+            // textBoxArea
+            // 
+            this.textBoxArea.Location = new System.Drawing.Point(321, 392);
+            this.textBoxArea.Name = "textBoxArea";
+            this.textBoxArea.Size = new System.Drawing.Size(118, 20);
+            this.textBoxArea.TabIndex = 3;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(463, 392);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(118, 20);
+            this.textBoxAddress.TabIndex = 4;
+            // 
+            // buttonGetapplication
+            // 
+            this.buttonGetapplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGetapplication.Location = new System.Drawing.Point(610, 377);
+            this.buttonGetapplication.Name = "buttonGetapplication";
+            this.buttonGetapplication.Size = new System.Drawing.Size(101, 49);
+            this.buttonGetapplication.TabIndex = 5;
+            this.buttonGetapplication.Text = "Ввести заявку";
+            this.buttonGetapplication.UseVisualStyleBackColor = true;
+            this.buttonGetapplication.Click += new System.EventHandler(this.buttonGetapplication_Click);
+            // 
+            // labelRooms
+            // 
+            this.labelRooms.AutoSize = true;
+            this.labelRooms.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelRooms.Location = new System.Drawing.Point(9, 374);
+            this.labelRooms.Name = "labelRooms";
+            this.labelRooms.Size = new System.Drawing.Size(135, 16);
+            this.labelRooms.TabIndex = 6;
+            this.labelRooms.Text = "Количество комнат";
+            // 
+            // labelFloor
+            // 
+            this.labelFloor.AutoSize = true;
+            this.labelFloor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFloor.Location = new System.Drawing.Point(208, 374);
+            this.labelFloor.Name = "labelFloor";
+            this.labelFloor.Size = new System.Drawing.Size(41, 16);
+            this.labelFloor.TabIndex = 7;
+            this.labelFloor.Text = "Этаж";
+            // 
+            // labelArea
+            // 
+            this.labelArea.AutoSize = true;
+            this.labelArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelArea.Location = new System.Drawing.Point(347, 374);
+            this.labelArea.Name = "labelArea";
+            this.labelArea.Size = new System.Drawing.Size(65, 16);
+            this.labelArea.TabIndex = 8;
+            this.labelArea.Text = "Площадь";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAddress.Location = new System.Drawing.Point(492, 374);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(47, 16);
+            this.labelAddress.TabIndex = 9;
+            this.labelAddress.Text = "Адрес";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Количество комнат";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1.Width = 120;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Этаж";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column2.Width = 120;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Площадь";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column3.Width = 120;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Адрес";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column4.Width = 120;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 450);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelArea);
+            this.Controls.Add(this.labelFloor);
+            this.Controls.Add(this.labelRooms);
+            this.Controls.Add(this.buttonGetapplication);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxArea);
+            this.Controls.Add(this.textBoxFloor);
+            this.Controls.Add(this.textBoxRooms);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Бюро обмена квартир \"Винзили\"";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxRooms;
+        private System.Windows.Forms.TextBox textBoxFloor;
+        private System.Windows.Forms.TextBox textBoxArea;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Button buttonGetapplication;
+        private System.Windows.Forms.Label labelRooms;
+        private System.Windows.Forms.Label labelFloor;
+        private System.Windows.Forms.Label labelArea;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
